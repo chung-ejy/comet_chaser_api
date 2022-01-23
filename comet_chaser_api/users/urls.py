36a955b919f75549e9cf7c3b8a9398802dcf4baa
chung-ejy <chung-ejy@gmail.com>
2022-01-23 15:32:48 -0500

    implemented auth

--- comet_chaser_api/users/urls.py
+++ comet_chaser_api/users/urls.py
@@ -5,8 +5,8 @@ from knox import views as knox_views
 
 urlpatterns = [
     path('auth/',include('knox.urls')),
-    path('auth/register/',RegisterApi.as_view()),
-    path('auth/login/',LoginApi.as_view()),
-    path('auth/user/',UserApi.as_view()),
-    path('auth/logout/',knox_views.LogoutView.as_view(),name="knox_logout")
+    path('register/',RegisterApi.as_view()),
+    path('login/',LoginApi.as_view()),
+    path('user/',UserApi.as_view()),
+    path('logout/',knox_views.LogoutView.as_view(),name="knox_logout")
 ]
